0c2172f7e7a90408131a736ad78202ab3aff17d2
Brandon Holt <brandon@synvoyity.com>
2022-04-12 16:09:43 -0400

    Added Terrain Generation
    Also standardized Planet prefab

--- Dolphin Place/Assets/Scripts/TerrainGenerator.cs
+++ Dolphin Place/Assets/Scripts/TerrainGenerator.cs
@@ -1,275 +1,103 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class TerrainGenerator : MonoBehaviour
 {
-    Mesh mesh;
-    Vector3[] vertices;
-    int[] triangles;
-    public int xSize = 100;
-    public int zSize = 100;
+    public Mesh planetMesh;
+    public Material waterMaterial;
+    public MeshRenderer waterRenderer;
     public Vector3 amplitude, frequency;
-    public float radius;
+    public Gradient surfaceGradient;
+    public Color waterColor;
 
-    // Start is called before the first frame update
+    private void Start()
+    {
+        GenerateTerrain();
+
+        SetWaterColor();
+    }
+
+#if UNITY_EDITOR
     void OnValidate()
     {
-        mesh = new Mesh();
+        GenerateTerrain();
 
-        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
-
-        IcoSphere.Create(mesh, radius, amplitude, frequency);
-
-        GetComponent<MeshFilter>().mesh = mesh;
-
-        //CreateShape(); UpdateMesh();
+        SetWaterColor();
     }
+#endif
 
-    void CreateShape()
+    private void GenerateTerrain()
     {
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        if (planetMesh == null) return;
 
-        for (int i = 0, z = 0; z <= zSize; z++)
-        {
-            for (int x = 0; x <= xSize; x++)
-            {
-                float y = amplitude.x * Mathf.PerlinNoise(x * frequency.x, z * frequency.x)
-                        + amplitude.y * Mathf.PerlinNoise(x * frequency.y, z * frequency.y)
-                        + amplitude.z * Mathf.PerlinNoise(x * frequency.z, z * frequency.z);
-                vertices[i] = new Vector3(x, y, z);
-                i++;
-            }
-        }
+        Mesh newMesh = new Mesh();
 
-        triangles = new int[xSize * zSize * 6];
+        Vector3[] vertices = planetMesh.vertices;
 
-        int vert = 0;
-        int tris = 0;
+        float[] heights = new float[vertices.Length];
 
-        for (int z = 0; z < zSize; z++)
-        {
-            for (int x = 0; x < xSize; x++)
-            {
-                triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
-                triangles[tris + 2] = vert + 1;
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 2;
-
-                vert++;
-                tris += 6;
-            }
-            vert++;
-        }
-
-    }
-
-    void UpdateMesh()
-    {
-        mesh.Clear();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateBounds();
-        mesh.RecalculateTangents();
-        mesh.RecalculateNormals();
-        // optionally, add a mesh collider (As suggested by Franku Kek via Youtube comments).
-        // To use this, your MeshGenerator GameObject needs to have a mesh collider
-        // component added to it.  Then, just re-enable the code below.
-        /*
-        mesh.RecalculateBounds();
-        MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
-        meshCollider.sharedMesh = mesh;
-        //*/
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (vertices == null)
-            return;
+        float minHeight = float.PositiveInfinity; float maxHeight = float.NegativeInfinity;
 
         for (int i = 0; i < vertices.Length; i++)
         {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
+            Vector3 v = vertices[i];
+
+            float phi = Mathf.Atan(Mathf.Sqrt((v.x * v.x) + (v.y * v.y)) / v.z);
+
+            float theta = Mathf.Atan2(v.y, v.x);
+
+            float x = phi / Mathf.PI;
+
+            float z = theta / (2 * Mathf.PI);
+
+            float noise = amplitude.x * Mathf.PerlinNoise(x * frequency.x, z * frequency.x)
+                        + amplitude.y * Mathf.PerlinNoise(x * frequency.y, z * frequency.y)
+                        + amplitude.z * Mathf.PerlinNoise(x * frequency.z, z * frequency.z);
+
+            vertices[i] *= 1f + (.1f * noise);
+
+            heights[i] = vertices[i].magnitude;
+
+            if (heights[i] < minHeight) minHeight = heights[i];
+
+            if (heights[i] > maxHeight) maxHeight = heights[i];
         }
-    }
-}
 
-public static class IcoSphere
-{
-    private struct TriangleIndices
-    {
-        public int v1;
-        public int v2;
-        public int v3;
+        Color[] colors = new Color[vertices.Length];
 
-        public TriangleIndices(int v1, int v2, int v3)
+        for (int i = 0; i < colors.Length; i++)
         {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.v3 = v3;
-        }
-    }
+            float normalizedHeight = Mathf.InverseLerp(minHeight, maxHeight, heights[i]);
 
-    // return index of point in the middle of p1 and p2
-    private static int getMiddlePoint(int p1, int p2, ref List<Vector3> vertices, ref Dictionary<long, int> cache, float radius)
-    {
-        // first check if we have it already
-        bool firstIsSmaller = p1 < p2;
-        long smallerIndex = firstIsSmaller ? p1 : p2;
-        long greaterIndex = firstIsSmaller ? p2 : p1;
-        long key = (smallerIndex << 32) + greaterIndex;
-
-        int ret;
-        if (cache.TryGetValue(key, out ret))
-        {
-            return ret;
+            colors[i] = surfaceGradient.Evaluate(normalizedHeight);
         }
 
-        // not in cache, calculate it
-        Vector3 point1 = vertices[p1];
-        Vector3 point2 = vertices[p2];
-        Vector3 middle = new Vector3
-        (
-            (point1.x + point2.x) / 2f,
-            (point1.y + point2.y) / 2f,
-            (point1.z + point2.z) / 2f
-        );
+        newMesh.vertices = vertices;
 
-        // add vertex makes sure point is on unit sphere
-        int i = vertices.Count;
-        vertices.Add(middle.normalized * radius);
+        newMesh.triangles = planetMesh.triangles;
 
-        // store it, return index
-        cache.Add(key, i);
+        newMesh.colors = colors;
 
-        return i;
+        newMesh.RecalculateNormals();
+
+        newMesh.RecalculateBounds();
+
+        newMesh.RecalculateTangents();
+
+        GetComponent<MeshFilter>().mesh = newMesh;
     }
 
-    public static void Create(Mesh mesh, float radius, Vector3 amplitude, Vector3 frequency)
+    private void SetWaterColor()
     {
-        mesh.Clear();
-        Vector3[] vertices = mesh.vertices;
-        List<Vector3> vertList = new List<Vector3>();
-        Dictionary<long, int> middlePointIndexCache = new Dictionary<long, int>();
-        //int index = 0;
+        if (waterMaterial == null || waterRenderer == null) return;
 
-        int recursionLevel = 3;
+        Material waterMaterialInstance = new Material(waterMaterial);
 
-        // create 12 vertices of a icosahedron
-        float t = (1f + Mathf.Sqrt(5f)) / 2f;
+        waterMaterialInstance.SetColor("_Water_Color", waterColor);
 
-        vertList.Add(new Vector3(-1f, t, 0f).normalized * radius);
-        vertList.Add(new Vector3(1f, t, 0f).normalized * radius);
-        vertList.Add(new Vector3(-1f, -t, 0f).normalized * radius);
-        vertList.Add(new Vector3(1f, -t, 0f).normalized * radius);
+        waterMaterialInstance.SetFloat("_Alpha", waterColor.a);
 
-        vertList.Add(new Vector3(0f, -1f, t).normalized * radius);
-        vertList.Add(new Vector3(0f, 1f, t).normalized * radius);
-        vertList.Add(new Vector3(0f, -1f, -t).normalized * radius);
-        vertList.Add(new Vector3(0f, 1f, -t).normalized * radius);
-
-        vertList.Add(new Vector3(t, 0f, -1f).normalized * radius);
-        vertList.Add(new Vector3(t, 0f, 1f).normalized * radius);
-        vertList.Add(new Vector3(-t, 0f, -1f).normalized * radius);
-        vertList.Add(new Vector3(-t, 0f, 1f).normalized * radius);
-
-
-        // create 20 triangles of the icosahedron
-        List<TriangleIndices> faces = new List<TriangleIndices>();
-
-        // 5 faces around point 0
-        faces.Add(new TriangleIndices(0, 11, 5));
-        faces.Add(new TriangleIndices(0, 5, 1));
-        faces.Add(new TriangleIndices(0, 1, 7));
-        faces.Add(new TriangleIndices(0, 7, 10));
-        faces.Add(new TriangleIndices(0, 10, 11));
-
-        // 5 adjacent faces 
-        faces.Add(new TriangleIndices(1, 5, 9));
-        faces.Add(new TriangleIndices(5, 11, 4));
-        faces.Add(new TriangleIndices(11, 10, 2));
-        faces.Add(new TriangleIndices(10, 7, 6));
-        faces.Add(new TriangleIndices(7, 1, 8));
-
-        // 5 faces around point 3
-        faces.Add(new TriangleIndices(3, 9, 4));
-        faces.Add(new TriangleIndices(3, 4, 2));
-        faces.Add(new TriangleIndices(3, 2, 6));
-        faces.Add(new TriangleIndices(3, 6, 8));
-        faces.Add(new TriangleIndices(3, 8, 9));
-
-        // 5 adjacent faces 
-        faces.Add(new TriangleIndices(4, 9, 5));
-        faces.Add(new TriangleIndices(2, 4, 11));
-        faces.Add(new TriangleIndices(6, 2, 10));
-        faces.Add(new TriangleIndices(8, 6, 7));
-        faces.Add(new TriangleIndices(9, 8, 1));
-
-
-        // refine triangles
-        for (int i = 0; i < recursionLevel; i++)
-        {
-            List<TriangleIndices> faces2 = new List<TriangleIndices>();
-            foreach (var tri in faces)
-            {
-                // replace triangle by 4 triangles
-                int a = getMiddlePoint(tri.v1, tri.v2, ref vertList, ref middlePointIndexCache, radius);
-                int b = getMiddlePoint(tri.v2, tri.v3, ref vertList, ref middlePointIndexCache, radius);
-                int c = getMiddlePoint(tri.v3, tri.v1, ref vertList, ref middlePointIndexCache, radius);
-
-                faces2.Add(new TriangleIndices(tri.v1, a, c));
-                faces2.Add(new TriangleIndices(tri.v2, b, a));
-                faces2.Add(new TriangleIndices(tri.v3, c, b));
-                faces2.Add(new TriangleIndices(a, b, c));
-            }
-            faces = faces2;
-        }
-
-        for (int i = 0; i < vertList.Count; i++)
-        {
-            vertList[i] *= Random.Range(1f, 1.3f);
-
-            // Vector3 v = vertList[i];
-
-            // float phi = Mathf.Atan(Mathf.Sqrt(v.x * v.x + v.y * v.y) / v.z);
-
-            // float theta = Mathf.Atan2(v.y, v.x);
-
-            // float x = phi / Mathf.PI;
-
-            // float z = theta / (2 * Mathf.PI);
-
-            // vertList[i] *= 1f + .2f * (amplitude.x * Mathf.PerlinNoise(x * frequency.x, z * frequency.x)
-            //             + amplitude.y * Mathf.PerlinNoise(x * frequency.y, z * frequency.y)
-            //             + amplitude.z * Mathf.PerlinNoise(x * frequency.z, z * frequency.z));
-        }
-
-        mesh.vertices = vertList.ToArray();
-
-        List<int> triList = new List<int>();
-        for (int i = 0; i < faces.Count; i++)
-        {
-            triList.Add(faces[i].v1);
-            triList.Add(faces[i].v2);
-            triList.Add(faces[i].v3);
-        }
-        mesh.triangles = triList.ToArray();
-        mesh.uv = new Vector2[vertices.Length];
-
-        Vector3[] normales = new Vector3[vertList.Count];
-        for (int i = 0; i < normales.Length; i++)
-            normales[i] = vertList[i].normalized;
-
-
-        mesh.normals = normales;
-
-        mesh.RecalculateBounds();
-        mesh.RecalculateTangents();
-        mesh.RecalculateNormals();
-        //mesh.Optimize();
+        waterRenderer.material = waterMaterialInstance;
     }
 }
